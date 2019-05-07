e2ce3b95cc7235064b867fc76d7a8788288933b0
robin <robintsunny@gmail.com>
2019-05-06 17:43:56 -0700

    leetCode myAtoi

--- practice.js
+++ practice.js
@@ -224,3 +224,64 @@ const reverse = (x) => {
     
     return res * neg
 };
+
+
+/*
+Implement atoi which converts a string to an integer.
+
+The function first discards as many whitespace characters 
+as necessary until the first non - whitespace character is 
+found.Then, starting from this character, takes an optional 
+initial plus or minus sign followed by as many numerical 
+digits as possible, and interprets them as a numerical 
+value.
+
+The string can contain additional characters after those 
+that form the integral number, which are ignored and have 
+no effect on the behavior of this
+function.
+
+If the first sequence of non - whitespace characters in str 
+is not a valid integral number, or if no such sequence 
+exists because either str is empty or it contains only 
+whitespace characters, no conversion is performed.
+
+If no valid conversion could be performed, a zero value is returned.
+*/
+const myAtoi = (str) => {
+    let res = 0
+        const min = Math.pow(2,31) * -1
+        const max = Math.pow(2,31) - 1
+        let nums = {
+            '0': 0, '1': 1, '2': 2, '3': 3, '4': 4, 
+            '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
+        }
+
+        // skip spaces
+        let i = 0
+        while (str[i] === ' ') {
+            i++
+        }
+    
+        // track negative
+        let neg = 1
+        if (str[i] === '-' || str[i] === '+') {
+            if (str[i] === '-') {
+                neg = -1
+            }
+            i++
+        }
+    
+        for( ; i<str.length; i++) {
+            if (!nums[str[i]] && str[i] !== '0') {
+                break
+            }
+
+            res = res*10 + neg*nums[str[i]]
+        }
+    
+    if (res > max) {return max}
+    if(res < min) {return min}
+    return res
+    
+};
